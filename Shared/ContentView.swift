a9d07e31e758dba6d27856eb584cd29a77d5cc86
Jacqueline Franz <jacquelinefranz1505@gmail.com>
2023-11-27 17:01:30 +0100

    Clean up for a fresh project

---
+++ Shared/ContentView.swift
@@ -0,0 +1,14 @@
+//
+//  ContentView.swift
+//  RezepteManager
+//
+//  Created by Jacqueline Franz on 27.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Placeholder")
+    }
+}
--- Shared/RezepteManagerApp.swift
+++ Shared/RezepteManagerApp.swift
@@ -6,19 +6,12 @@
 //
 
 import SwiftUI
-let appName = "Rezepte Manager"
 
 @main
 struct RezepteManagerApp: App {
-    let persistenceController = PersistenceController.shared
     var body: some Scene {
         WindowGroup {
             
-            NavigationView {
-                Sidebar()
-                Home()
-                    .environment(\.managedObjectContext, persistenceController.container.viewContext)
-            }
         }
     }
 }
